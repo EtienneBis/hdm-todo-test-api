f78b072aec49c19996e4cb554d32f8a42deee9ed
EtienneBis <165167093+EtienneBis@users.noreply.github.com>
2025-02-04 00:22:14 +0100

    Update DeleteTask.ts

--- src/UseCase/DeleteTask/DeleteTask.ts
+++ src/UseCase/DeleteTask/DeleteTask.ts
@@ -4,14 +4,12 @@ import TaskRepository from '../../Repositories/TaskRepository';
 
 @Injectable()
 export default class DeleteTask
-  implements UseCase<Promise<boolean>, [id: number]>
-{
+  implements UseCase<Promise<boolean>, [id: number]>{
   constructor(private readonly taskRepository: TaskRepository) {}
 
   async handle(id: number) {
     try {
       await this.taskRepository.delete(id);
-
       return true;
     } catch (error) {
       throw new BadRequestException(error.message);
