0f248da29c577a9044bf4f0b46067c36cc3118f4
EtienneBis <165167093+EtienneBis@users.noreply.github.com>
2025-02-03 17:52:47 +0100

    Update PrismaService.ts

--- src/PrismaService.ts
+++ src/PrismaService.ts
@@ -1,9 +1,10 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import {Injectable, OnModuleDestroy, OnModuleInit} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
+
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   constructor(private readonly configService: ConfigService) {
     super({
       datasources: {
@@ -23,7 +24,7 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
   }
-  
   async onModuleDestroy() {
     await this.$disconnect();
+  }
 }
