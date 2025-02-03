8e152f2269b800b9ba989d4b75a88f60407de1d0
EtienneBis <165167093+EtienneBis@users.noreply.github.com>
2025-02-03 18:03:41 +0100

    Update TaskController.ts

--- src/Controllers/TaskController.ts
+++ src/Controllers/TaskController.ts
@@ -14,11 +14,14 @@ import SaveTaskDto from '../UseCase/SaveTask/SaveTaskDto';
 import UseCaseFactory from '../UseCase/UseCaseFactory';
 import TaskRepository from "../Repositories/TaskRepository";
 import SaveTaskUseCase from "../UseCase/SaveTask/SaveTaskUseCase";
+import UpdateTaskUseCase from "../UseCase/UpdateTask/UpdateTaskUseCase"; // Import du use case de mise à jour
 
 @Controller()
 export default class TaskController {
-  constructor(private readonly useCaseFactory: UseCaseFactory,
-              private readonly taskRepository: TaskRepository,) {}
+  constructor(
+    private readonly useCaseFactory: UseCaseFactory,
+    private readonly taskRepository: TaskRepository,
+  ) {}
 
   @Get('/tasks')
   async getAll() {
@@ -27,22 +30,23 @@ export default class TaskController {
 
   @Post('/tasks')
   async create(@Body() dto: SaveTaskDto) {
-    console.log('Received task data:', dto); 
-    return (await this.useCaseFactory.create(SaveTaskUseCase)).handle(dto);
+    console.log('Received task data:', dto);
+    return (await this.useCaseFactory.create<SaveTaskUseCase>(SaveTaskUseCase)).handle(dto);
   }
 
   @Patch('/tasks/:id')
-  async update(@Param('id') id: string,@Body() dto: SaveTaskDto) {
+  async update(@Param('id') id: string, @Body() dto: SaveTaskDto) {
     if (!dto.name) {
       throw new BadRequestException('The name field is required.');
     }
-    const saveTaskUseCase = await this.useCaseFactory.create(SaveTaskUseCase);
+    const updateTaskUseCase = await this.useCaseFactory.create<UpdateTaskUseCase>(UpdateTaskUseCase);
+
     dto.id = Number(id);
-    return saveTaskUseCase.handle(dto);
+    return updateTaskUseCase.handle(dto);
   }
 
   @Delete('/tasks/:id')
   async delete(@Param('id') id: string) {
-    return await ((await this.useCaseFactory.create(DeleteTask)).handle(Number(id)));
+    return await (await this.useCaseFactory.create(DeleteTask)).handle(Number(id));
   }
 }
